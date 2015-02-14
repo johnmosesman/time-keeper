4903284088be0785b69fa1da9637315140794389
jmosesman <jmosesman@ou.edu>
2015-02-14 17:29:15 -0600

    Add token authentication for Ember Simple Auth with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :records, except: [:new, :edit]
   resources :categories, except: [:new, :edit]
-  devise_for :users
+
+  devise_for :users, controllers: { sessions: 'sessions' }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
